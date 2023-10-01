8f09347781037ecd1ede70dba67810b9afa90fb9
gmenegoz <gmenegoz@gmail.com>
2023-10-01 20:58:01 +0200

    feat: scaffold for the extension with tentative translation machinery

--- scratch-vm/src/extensions/scratch3_tuio/index.js
+++ scratch-vm/src/extensions/scratch3_tuio/index.js
@@ -3,19 +3,43 @@ const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
 
-//const TuioClient = require('../../../../tuio-client/');
+const formatMessage = require('format-message');
+//https://github.com/xcratch/xcx-example/blob/main/src/block/translations.json
+const transl = {
+    "hr": { 
+        "tuioConnect": "Connect TUIO HR"
+    },
+    "it": {
+        "tuioConnect": "Connetti a TUIO"
+    },
+    "en": { 
+        "tuioConnect": "Connect TUIO"
+    },
+    "fr": { 
+        "tuioConnect": "Connect TUIO FR"
+    }
+}
 
-//const client = new TuioClient({host:'ws://localhost:8080'});
-//client.on("connect", function() {
- //   isConnected = true;
- //   log.log("connected!");
-//});
+formatMessage.setup({
+    translations: transl
+});
+
+const TuioClient = require('tuio-client');
+
+let isConnected = false;
+
+const client = new TuioClient({host:'ws://localhost:8080'});
+client.on("connect", function() {
+   isConnected = true;
+   log.log("connected!");
+});
 
 
 class Scratch3Tuio {
     constructor(runtime) {
         this.runtime = runtime;
-
+            
+   
         /**
          * A toggle that alternates true and false each frame, so that an
          * edge-triggered hat can trigger on every other frame.
@@ -37,15 +61,22 @@ class Scratch3Tuio {
                 {
                     opcode: 'connect',
                     blockType: BlockType.COMMAND,
-                    text: 'connect TUIO',
+                    text: formatMessage({
+                        id: 'tuioConnect',
+                        default : 'connect TUIO',
+                        description: 'connect to TUIO server'
+                    }),
                     arguments: {}
                 }
             ]
         }
     }
 
-    connect () {
-        console.log('Connect!');
+    connect() {
+        if(isConnected) {
+            return;
+        }
+        client.connect();
     }
 
 }
