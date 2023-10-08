bb1ddef76abb0a09a6c7d0868b96a9f8d0586f33
gmenegoz <gmenegoz@gmail.com>
2023-10-08 11:09:30 +0200

    test: failing unit test added for tuio extension

---
+++ scratch-vm/test/unit/extension_tuio.js
@@ -0,0 +1,13 @@
+const test = require('tap').test;
+// const Tuio = require('../../src/extensions/scratch3_tuio/index.js');
+
+
+/* const fakeRuntime = {
+    getTargetForStage: () => ({}),
+    on: () => {} // Stub out listener methods used in constructor.
+}; */
+
+test('a failing test', t => {
+    t.equal(0, 1);
+    t.end();
+});
