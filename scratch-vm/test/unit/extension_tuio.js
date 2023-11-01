77ab30ccf0afaf24a1bb7660940e431ebc7cc010
gmenegoz <gmenegoz@gmail.com>
2023-11-01 18:56:41 +0100

    feat: new block added to follow a marker

--- scratch-vm/test/unit/extension_tuio.js
+++ scratch-vm/test/unit/extension_tuio.js
@@ -23,6 +23,51 @@ test('isConnected variable correctly describes state', t => {
     t.end();
 });
 
+test('rescaling of X coordinates works correctly', t => {
+    const farLeftRaw = 0;
+    const halfwayRaw = 0.5;
+    const farRightRaw = 1;
+    const farLeftScratch = -240;
+    const halfwayScratch = 0;
+    const farRightScratch = 240;
+    t.equal(tuio.rescaleX(farLeftRaw), farLeftScratch);
+    t.equal(tuio.rescaleX(halfwayRaw), halfwayScratch);
+    t.equal(tuio.rescaleX(farRightRaw), farRightScratch);
+    t.end();
+});
+
+test('rescaling of Y coordinates works correctly', t => {
+    const farUpRaw = 0;
+    const halfwayRaw = 0.5;
+    const farDownRaw = 1;
+    const farUpScratch = 180;
+    const halfwayScratch = 0;
+    const farDownScratch = -180;
+    t.equal(tuio.rescaleY(farUpRaw), farUpScratch);
+    t.equal(tuio.rescaleY(halfwayRaw), halfwayScratch);
+    t.equal(tuio.rescaleY(farDownRaw), farDownScratch);
+    t.end();
+});
+
+test('rescaling of Angle coordinates works correctly', t => {
+    const facingUpRaw = 0;
+    const facingRightRaw = Math.PI / 2;
+    const facingDownRaw = Math.PI;
+    const facingUpScratch = 0;
+    const facingRightScratch = 90;
+    const facingDownScratch = 180;
+    t.equal(tuio.rescaleAngle(facingUpRaw), facingUpScratch);
+    t.equal(tuio.rescaleAngle(facingRightRaw), facingRightScratch);
+    t.equal(tuio.rescaleAngle(facingDownRaw), facingDownScratch);
+    t.end();
+});
+
+/* test('follow marker works correctly'), t => {
+    tuio.followMarkerWithID(args, util)
+    t.equal(util.target.x, 100);
+    t.end();
+}); */
+
 /* test('marker object contains all data', t => {
     const marker = new Marker();
     const markerObject = Tuio._ma
